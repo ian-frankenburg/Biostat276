3f849e1f6cebc719b05dd3fd91814e9df58c7afb
Ian Frankenburg <31015994+ian-frankenburg@users.noreply.github.com>
2020-04-26 21:34:08 -0700

    started working on mcmc entirely in c++

--- HW1/helperFunctions.cpp
+++ HW1/helperFunctions.cpp
@@ -7,51 +7,55 @@ using namespace std;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 
-double loglambdaTarget(double lambda, vector<double> tau2, double a, double b) {
-  return((-a-1)*log(lambda)-(pow(lambda,2)/2*accumulate(tau2.begin(),tau2.end(),0))-1/(b*lambda));
+const arma::mat loglambdaTarget(const arma::mat lambda, const arma::vec tau2, double a, double b) {
+  return((-a-1)*log(lambda)-(pow(lambda,2)/2*arma::accu(tau2)-1/(b*lambda)));
 }
 
-double logtau2jTarget(double tau2j, double lambda, double betaj){
+const arma::mat logtau2jTarget(const arma::mat tau2j, const arma::mat lambda, const arma::mat betaj){
   return(-log(sqrt(tau2j)) - 1.0/2.0*(1.0/tau2j*pow(betaj,2) + pow(lambda,2)*tau2j));
 }
 
-// [[Rcpp::export]]
-double lambdaDraw(double current, vector<double> tau2, double a, double b){
-  std::random_device rd;
-  std::mt19937 mt(rd());
-  std::uniform_real_distribution<double> dist(0, 1.0);
-  std::normal_distribution<double> norm(0, current);
-  double proposed = exp(log(current)+norm(mt));
-  double logr = loglambdaTarget(proposed,tau2,1,b)-
+const arma::mat lambdaDraw(const arma::mat current, const arma::vec tau2, double a, double b){
+  //std::random_device rd;
+  //std::mt19937 mt(rd());
+  //std::uniform_real_distribution<double> dist(0, 1.0);
+  // std::normal_distribution<double> norm(0, current);
+  const arma::mat e = arma::randn(1) * sqrt(current);
+  const arma::mat u = arma::randu(1);
+  const arma::mat proposed = exp(log(current)+e);
+  const arma::mat logr = loglambdaTarget(proposed,tau2,1,b)-
     loglambdaTarget(current,tau2,1,b)+log(proposed)-
     log(current);
-  if(log(dist(mt))<logr){
+  bool go=log(u)<logr;
+  if(go){
     current = proposed;
   }
   return current;
 }
 
-// [[Rcpp::export]]
-vector<double> tau2Draw(vector<double> current, vector<double> beta, double lambda){
-  std::random_device rd;
-  std::mt19937 mt(rd());
-  std::uniform_real_distribution<double> dist(0, 1.0);
-  std::normal_distribution<double> norm(0, 1);
+const arma::vec tau2Draw(const arma::vec current, const arma::vec beta, const arma::mat lambda){
+  // std::random_device rd;
+  // std::mt19937 mt(rd());
+  // std::uniform_real_distribution<double> dist(0, 1.0);
+  // std::normal_distribution<double> norm(0, 1);
+  const arma::mat e = arma::randn(1);
+  const arma::mat u = arma::randu(1);
+  const arma::mat proposed = exp(log(current)+e);
+  
   for(int j=0;j < current.size(); j++){
     double tau2j_proposed = exp(log(current[j])+norm(mt));
     double logr =
       logtau2jTarget(tau2j_proposed, lambda, beta[j]) -
       logtau2jTarget(current[j], lambda, beta[j]) +
       log(tau2j_proposed)-log(current[j]);
-    if(log(dist(mt)) < logr){
+    if(log(dist(u)) < logr){
       current[j] = tau2j_proposed;
     }
   }
   return current;
 }
 
-// [[Rcpp::export]]
-double sigma2Draw(const arma::vec & beta, const arma::vec & y, const arma::mat & X){
+const arma::mat sigma2Draw(const arma::vec & beta, const arma::vec & y, const arma::mat & X){
   std::random_device rd;
   std::mt19937 mt(rd());
   int n = X.n_rows;
@@ -62,7 +66,6 @@ double sigma2Draw(const arma::vec & beta, const arma::vec & y, const arma::mat &
   return 1.0/gamma(mt);
 }
 
-// [[Rcpp::export]]
 List betaMeanCov(const arma::vec & y, const arma::mat & X, double sigma2, const arma::vec & tau2){
   std::random_device rd;
   std::mt19937 mt(rd());
@@ -74,10 +77,50 @@ List betaMeanCov(const arma::vec & y, const arma::mat & X, double sigma2, const 
   return List::create(Named("cov") = M, Named("mean")= m);
 }
 
+arma::mat rmvnorm_cpp(int n,
+                  const arma::vec& mu,
+                  const arma::mat& Sigma) {
+  unsigned int p = Sigma.n_cols;
+  // First draw N x P values from a N(0,1)
+  Rcpp::NumericVector draw = Rcpp::rnorm(n*p);
+  // Instantiate an Armadillo matrix with the // drawn values using advanced constructor // to reuse allocated memory
+  arma::mat Z = arma::mat(draw.begin(), n, p,
+                          false, true); // Simpler, less performant alternative
+  // arma::mat Z = Rcpp::as<arma::mat>(draw);
+  // Generate a sample from the Transformed // Multivariate Normal
+  arma::mat Y = arma::repmat(mu, 1, n).t() +
+    Z * arma::chol(Sigma);
+  return Y;
+}
 
-// List mcmc(double lambda, double sigma2, const arma::vec & tau2, const arma::vec & beta2){
-//   
-//   return List::create(Named("beta.chain") = M, Named("sigma2.chain")= m,
-//                       Named("tau2.chain") = M, Named("lambda.chain") = M);
-// }
-
+// [[Rcpp::export]]
+List mcmc(double numSamples, const arma::vec & y, const arma::mat & X){
+  int p = X.n_cols;
+  
+  arma::vec lambdaChain = arma::ones(numSamples);
+  arma::vec sigma2Chain = arma::ones(numSamples);
+  arma::mat tau2Chain = arma::zeros(p,numSamples);
+  arma::mat betaChain = arma::zeros(p,numSamples);
+  
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  arma::mat I = arma::eye(p,p);
+  arma::mat tau2_inv = arma::eye(p,p);
+  arma::mat M = arma::eye(p,p);
+  arma::colvec m = arma::ones(p);
+  
+  double lambda = 1; double sigma2 = 1;
+  const arma::vec beta=arma::zeros(p); const arma::vec tau2=arma::ones(p);
+  
+  for(int s=0; s<numSamples; s++){
+    lambda = lambdaDraw(lambda, tau2, 1, 1);
+    tau2 = tau2Draw(tau2, beta, lambda);
+    sigma2 = sigma2Draw(beta, y, X);
+    tau2_inv = arma::inv(arma::diagmat(tau2));
+    M = arma::inv(X.t()*X/sigma2+tau2_inv);
+    m = M*X.t()*y/sigma2;
+    beta = rmvnorm_cpp(1,m,M);
+  }
+  return List::create(Named("beta.chain") = betaChain, Named("sigma2.chain")= sigma2Chain,
+                      Named("tau2.chain") = tau2Chain, Named("lambda.chain") = lambdaChain);
+}

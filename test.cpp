83f10fb0c94243256516019b910ad917fc04fb6d
Ian Frankenburg <31015994+ian-frankenburg@users.noreply.github.com>
2020-04-27 23:16:51 -0700

    fixing arma types

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+arma::vec getEigenValues(arma::mat M) {
+  return arma::eig_sym(M);
+}
+
+/*** R
+set.seed(42)
+X <- matrix(rnorm(4*4), 4, 4)
+Z <- X %*% t(X)
+getEigenValues(Z)
+*/
